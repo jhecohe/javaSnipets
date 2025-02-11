f40190a3c16cb9949bbf95231ae6a6a552f2aab2
jhecohe <jhecohe@jhecohes-MacBook-Pro.local>
2025-02-11 10:01:04 -0500

    Se finalizo script API

--- APIs/ConsumeAPI11Plus.java
+++ APIs/ConsumeAPI11Plus.java
@@ -1,48 +1,47 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ConsumeAPI11Plus {
 
     public static void main(String[] args) {
         HttpClient hc = HttpClient.newHttpClient();
         HttpRequest hr = HttpRequest.newBuilder()
-            .uri(URI.create("https://jsonplaceholder.typicode.com/posts/" + 1))
+            .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
             .GET()
             .build();
 
-        Post p = new Post("12", "`1322", "title", "body");
-
-        System.out.println("Test post => " + p.toString());
-
         try {
-            // HttpResponse<String> hres = hc.send(
-            //     hr,
-            //     HttpResponse.BodyHandlers.ofString()
-            // );
-            // System.out.println("Post => " + hres.body());
-            // From above is working to return a String
-            //
-
             HttpResponse<InputStream> hres = hc.send(
                 hr,
-                BodyHandlers.ofInputStream()
+                HttpResponse.BodyHandlers.ofInputStream()
+                // HttpResponse.BodyHandlers.ofString()
             );
-            ObjectInputStream ois = new ObjectInputStream(hres.body());
-            Post post = (Post) ois.readObject();
 
-            System.out.println(post.toString());
+            InputStreamReader isr = new InputStreamReader(hres.body());
+            BufferedReader br = new BufferedReader(isr);
+
+            String linea = "";
+            int count = 0;
+            do {
+                linea = br.readLine();
+                if (linea != null && linea.contains("id\":")) {
+                    count++;
+                    System.out.println(linea);
+                    System.out.println(linea.substring(linea.length() - 3));
+                }
+            } while (linea != null);
+            System.out.println("Total " + count);
         } catch (IOException ioe) {
             System.out.println("Error consuming API =>" + ioe);
         } catch (InterruptedException ie) {
             System.out.println("Interrupted => " + ie);
-        } catch (ClassNotFoundException cne) {
-            System.out.println("Interrupted => " + cne);
         }
     }
 }
