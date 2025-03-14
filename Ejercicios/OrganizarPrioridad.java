2fafca722771a268f0f9f0302814080b1eeab65c
jhecohe <jhecohe@gmail.com>
2025-03-14 16:12:19 -0500

    Ejercicios agregados

---
+++ Ejercicios/OrganizarPrioridad.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+
+class OrganizarPrioridad {
+
+    //Organizar = ['1001:1','1002:1','1002:2','1003:3','1004:4']
+    // Se debe organizar por el primer valor, en caso de haber dos con el mismo valor,se
+    // da prioridad con el segundo valor
+
+    public static void main(String[] args) {
+        // Código para organizar prioridades
+
+        String[] valores = { "1001:1", "1002:1", "1002:2", "1003:3", "1004:4" };
+
+        PriorityQueue pq = new PriorityQueue<>();
+    }
+}
